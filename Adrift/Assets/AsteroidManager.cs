352c5ca3e33d592449e46b661c7e4284e1f5e832
Ben Jukes (biggalsworth) <107201316+biggalsworth@users.noreply.github.com>
2024-06-03 15:02:16 +0100

    added asteroid spawning

--- Adrift/Assets/AsteroidManager.cs
+++ Adrift/Assets/AsteroidManager.cs
@@ -1,15 +1,32 @@
+using NUnit.Framework;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AsteroidManager : MonoBehaviour
 {
-    public List<GameObject> asteroids;
+    public List<GameObject> asteroidPrefabs;
+
+    private List<GameObject> asteroids = new List<GameObject>();
+
+    public int asteroidAmount = 50;
+
     //public List<GameObject> asteroids;
 
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log(asteroidPrefabs.Count - 1);
+        while (asteroids.Count < asteroidAmount)
+        {
+            GameObject tempRoid = Instantiate(asteroidPrefabs[UnityEngine.Random.Range(0, asteroidPrefabs.Count)], gameObject.transform);
+            tempRoid.AddComponent<AsteroidClass>();
+            tempRoid.SetActive(false);
+            asteroids.Add(tempRoid);
+        }
+
+        DefaultAsteroids();
         
     }
 
@@ -19,11 +36,50 @@ public class AsteroidManager : MonoBehaviour
         
     }
 
-    public void GetAsteroid()
+    public GameObject GetAsteroid()
     {
-        foreach (GameObject roid in asteroids)
+        for(int i = 0; i < asteroids.Count; i++)
         {
+            GameObject tempRoid = asteroids[i];
+            if (tempRoid.GetComponent<AsteroidClass>().available)
+            {
+                //GameObject tempRoid = asteroids[index];
+                asteroids.RemoveAt(i);
+                asteroids.Add(tempRoid);
 
+                tempRoid.SetActive(true);
+                tempRoid.GetComponent<AsteroidClass>().available = false;
+                return tempRoid;
+            }
+        }
+        return null;
+    }
+
+    private void DefaultAsteroids()
+    {
+        GameObject Ship = GameObject.Find("Ship");
+        for (int i = 0; i < 20; i++)
+        {
+            GameObject tempObj = GetAsteroid();
+            if (tempObj != null)
+            {
+                Vector3 randPos = new Vector3(Ship.transform.position.x + UnityEngine.Random.Range(-50, 50), Ship.transform.position.y + UnityEngine.Random.Range(-50, 50), Ship.transform.position.z + UnityEngine.Random.Range(-50, 50));
+                if (Vector3.Distance(Ship.transform.position, randPos) > 50)
+                {
+                    RaycastHit hit;
+                    if (Physics.SphereCast(randPos, 20, transform.forward, out hit, 20))
+                    {
+                        //spawned in another object;
+                        return;
+                    }
+                    else
+                    {
+                        tempObj.transform.position = randPos;
+                    }
+
+                }
+
+            }
         }
     }
 }
--- Adrift/Assets/ObjectGeneration.cs
+++ Adrift/Assets/ObjectGeneration.cs
@@ -5,22 +5,52 @@ using UnityEngine;
 public class ObjectGeneration : MonoBehaviour
 {
     public GameObject Ship;
+    private float spawnDist;
     private BasicDrive shipStats;
 
+    public AsteroidManager asteroidController; 
+    [Range(0, 1)]
+    public float asteroidProbability;
+
     public List<GameObject> objects;
+
 
     // Start is called before the first frame update
     void Start()
     {
         shipStats = Ship.GetComponent<BasicDrive>();
+        spawnDist = Mathf.Abs(GameObject.Find("SpawnDistance").GetComponent<SphereCollider>().radius + 50);
+
     }
 
-    // Update is called once per frame
-    void Update()
+// Update is called once per frame
+void Update()
     {
-        if(shipStats.currVelocity > 20.0f)
+        if (shipStats.currVelocity > 1f)
         {
-            //Time.timeScale = 0.5f;
+            if (Random.Range(0, 1) <= asteroidProbability)
+            {
+                GameObject tempObj = asteroidController.GetAsteroid();
+                if (tempObj != null)
+                {
+                    Vector3 randPos = new Vector3(Ship.transform.position.x + Random.Range(-spawnDist, spawnDist), Ship.transform.position.y + Random.Range(-spawnDist, spawnDist), Ship.transform.position.z + Random.Range(-spawnDist, spawnDist));
+                    if(Vector3.Distance(Ship.transform.position, randPos) > spawnDist - 50)
+                    {
+                        RaycastHit hit;
+                        if(Physics.SphereCast(randPos, 20, transform.forward, out hit, 20))
+                        {
+                            //spawned in another object;
+                            return;
+                        }
+                        else
+                        {
+                            tempObj.transform.position = randPos;
+                        }
+
+                    }
+
+                }
+            }
         }
     }
 }
